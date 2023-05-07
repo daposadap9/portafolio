49126b2501806dc9e710ed2b9f5b782643f2d587
daposadap <diegoposada1999@gmail.com>
2023-05-07 10:56:48 -0500

    new changes

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
-import { Link, } from "react-router-dom";
+import { NavLink, } from "react-router-dom";
 import { faEnvelope, faHome, faTasks, faUser} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
@@ -36,14 +36,14 @@ const Nav = ({ handleActive }) => {
       <ul className="flex flex-col items-center justify-center gap-2 text-2xl font-dashiellText italic font-black 
       first:text-2xl midMobile:text-3xl tablet:text-4xl translate-y-[-20px]">
         <motion.div
-        initial={{ x: -1000 }}
+        initial={{ x: -2000 }}
         animate={{ x: 0 }}
         transition={{ duration: 1}}
         className="text-center mb-4 mt-4 px-12 mx-2 text-xl">
         <p>Follow us on our social media</p>
         </motion.div>
         <motion.div className="flex w-screen first:justify-around midMobile:justify-evenly first:px-20 justify-around px-10 text-gray"
-        initial={{ x: -1000 }}
+        initial={{ x: -2000 }}
         animate={{ x: 0 }}
         transition={{ duration: 1.2}}>
         <li><FontAwesomeIcon icon={faInstagram}/></li>
@@ -51,50 +51,50 @@ const Nav = ({ handleActive }) => {
         <li><FontAwesomeIcon icon={faFacebook}/></li>
       </motion.div>
         <motion.li
-        initial={{ x: -1000 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.6}}
-        className="px-4 py-0 m-0 text-center max-w-xs"
-        ref={containerAnimation}>
-        </motion.li> 
-        
-        <motion.li
-        initial={{ x: -1000 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.6}}
-        className="px-4 py-2 text-center sombras text-white w-[50%]">
-          <Link className="flex flex-row gap-2 justify-center items-center" onClick={handleActive} to="/">
-            <FontAwesomeIcon icon={faHome}/><h1>Home</h1>
-          </Link>
-        </motion.li>
-        <motion.li 
-        initial={{ x: -1000 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.8}}
-        className="px-4 py-2 sombras text-white w-[50%]">
-          <Link className="flex flex-row gap-2 justify-center items-center" onClick={handleActive} to="/projects">
-          <FontAwesomeIcon icon={faTasks}/><h1>Projects</h1>
-          </Link>
-        </motion.li>
-        <motion.li
-        initial={{ x: -1000 }}
+        initial={{ x: -2000 }}
         animate={{ x: 0 }}
         transition={{ duration: 1}}
-        className="px-4 py-2  sombras text-white">
-          <Link className="flex flex-row gap-2 justify-center items-center" to="/">
-          <FontAwesomeIcon icon={faUser}/><h1>About me</h1>
-          </Link>
+        className="px-4 py-0 m-0 text-center max-w-xs"
+        ref={containerAnimation}>
+        </motion.li>
+        
+        <motion.li
+        initial={{ x: -2000 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.8}}
+        className="sombras text-white shadow-xl w-full overflow-hidden">
+          <NavLink className={({isActive}) => isActive?"bg-morado flex flex-row gap-2 justify-center items-center py-1":"bg-[#C6C8EE] flex flex-row gap-2 justify-center items-center py-1 hover:bg-morado"} onClick={handleActive} to="/">
+            <FontAwesomeIcon icon={faHome}/><h1>Home</h1>
+          </NavLink>
+        </motion.li>
+        <motion.li 
+        initial={{ x: -2000 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.8}}
+        className="sombras text-white   shadow-xl w-full overflow-hidden">
+          <NavLink className={({isActive}) => isActive?"bg-morado flex flex-row gap-2 justify-center items-center py-1":"bg-[#C6C8EE] flex flex-row gap-2 justify-center items-center py-1 hover:bg-morado"} onClick={handleActive} to="/projects">
+          <FontAwesomeIcon icon={faTasks}/><h1>Projects</h1>
+          </NavLink>
         </motion.li>
         <motion.li
-        initial={{ x: -1000 }}
+        initial={{ x: -2000 }}
         animate={{ x: 0 }}
-        transition={{ duration: 1.2}}
-        className="px-4 py-2  sombras text-white">
-          <Link className="flex flex-row gap-2 justify-center items-center" to="/">
-          <FontAwesomeIcon icon={faEnvelope}/><h1>Contact me</h1>
-          </Link>
+        transition={{ duration: 0.8}}
+        className=" sombras text-white shadow-xl w-full overflow-hidden">
+          <NavLink className="bg-[#C6C8EE] flex flex-row gap-2 justify-center items-center py-1 hover:bg-morado" onClick={handleActive} to="/">
+          <FontAwesomeIcon icon={faUser}/><h1>About me</h1>
+          </NavLink>
         </motion.li>
-      <motion.div initial={{ x: -1000 }}
+        <motion.li
+        initial={{ x: -2000 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.8}}
+        className=" sombras text-white shadow-xl w-full overflow-hidden">
+          <NavLink className="bg-[#C6C8EE] flex flex-row gap-2 justify-center items-center py-1 hover:bg-morado" onClick={handleActive} to="/">
+          <FontAwesomeIcon icon={faEnvelope}/><h1>Contact me</h1>
+          </NavLink>
+        </motion.li>
+      <motion.div initial={{ x: -2000 }}
         animate={{ x: 0 }}
         transition={{ duration: 1.3}}
       className="text-center relative text-xl px-5">
